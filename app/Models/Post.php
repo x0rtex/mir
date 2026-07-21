a659a59e5a30a1178b2700375d8984d03cc716b1
x0rtex <dev@x0rtex.xyz>
2026-07-21 17:15:15 +0100

    fix: fillable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-#[Fillable(['title', 'slug', 'body', 'excerpt', 'published_at'])]
+#[Fillable(['title', 'slug', 'body', 'excerpt', 'published_at', 'user_id'])]
 class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
